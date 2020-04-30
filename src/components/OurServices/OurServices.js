65d23309439f5319b7f92a5172b412fe1f5c0261
dammyayans <dammyayans560@gmail.com>
2020-04-30 02:16:40 -0700

    adding animations to other sections on the home page

--- src/components/OurServices/OurServices.js
+++ src/components/OurServices/OurServices.js
@@ -1,62 +1,72 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
+import Slide from "react-reveal/Slide";
+import Zoom from "react-reveal/Zoom";
 
-import './OurServices.css';
-import { Container, Col, Row } from 'react-bootstrap';
-
-
+import "./OurServices.css";
+import { Container, Col, Row } from "react-bootstrap";
 
 export default class OurServices extends Component {
-    render() {
-        return (
-            <Container fluid className="greybg">
-                <Container className="a-our-services-wrapper">
-                    <Row className="justify-content-md-center">
-                        <Col md="auto">
-                            <h2 className="bigText">
-                                <span className="blackText">Our </span>
-                                <span className="green">Services</span>
-                            </h2>
-                        </Col>
-                    </Row>
-                    <Row className="justify-content-md-center">
-                        <Col xs={12} md={4} className="a-our-services-heading">
-                            <h4 className="mediumText blackText">Branding</h4>
-                            <p className="blackText montserrat smallestText">
-                                We give your product and brand a <br/>
-                                distinctive look, unveiling the grasp of what <br/>
-                                your brand does, and what it stands for.
-                            </p>
-                        </Col>
-                        <Col xs={12} md={4} className="a-our-services-heading">
-                            <h4 className="mediumText blackText">Product Design</h4>
-                            <p className="blackText montserrat smallestText">
-                                We give your website a beautiful look, <br/>
-                                understandable and easy-to-use interface <br/>
-                                for your customers.
-                            </p>
-                        </Col>
-                    </Row>
-                    <Row className="justify-content-md-center">
-                        <Col xs={12} md={4} className="a-our-services-heading">
-                            <h4 className="mediumText blackText">Content Marketing</h4>
-                            <p className="blackText montserrat smallestText">
-                            We deliver the perfect message to the <br/>
-                            perfect listeners. We write blogs, stories, <br/>
-                            newsletter and needed captions.
-                            </p>
-                        </Col>
-                        <Col xs={12} md={4} className="a-our-services-heading">
-                            <h4 className="mediumText blackText">Software Development</h4>
-                            <p className="blackText montserrat smallestText">
-                            We develop your apps and websites into a <br/>
-                            perfect working prototype as we deliver <br/>
-                            design, front-end and back-end service.
-                        </p>
-                        </Col>
-                    </Row>
-                </Container>
-            </Container>
-        )
-    }
+  render() {
+    return (
+      <Container fluid className="greybg">
+        <Container className="a-our-services-wrapper">
+          <Row className="justify-content-md-center">
+            <Col md="auto">
+              <Slide top>
+                <h2 className="bigText">
+                  <span className="blackText">Our </span>
+                  <span className="green">Services</span>
+                </h2>
+              </Slide>
+            </Col>
+          </Row>
+          <Row className="justify-content-md-center">
+            <Col xs={12} md={4} className="a-our-services-heading">
+              <Zoom left delay={500}>
+                <h4 className="mediumText blackText">Branding</h4>
+                <p className="blackText montserrat smallestText">
+                  We give your product and brand a <br />
+                  distinctive look, unveiling the grasp of what <br />
+                  your brand does, and what it stands for.
+                </p>
+              </Zoom>
+            </Col>
+            <Col xs={12} md={4} className="a-our-services-heading">
+              <Zoom right delay={500}>
+                <h4 className="mediumText blackText">Product Design</h4>
+                <p className="blackText montserrat smallestText">
+                  We give your website a beautiful look, <br />
+                  understandable and easy-to-use interface <br />
+                  for your customers.
+                </p>
+              </Zoom>
+            </Col>
+          </Row>
+          <Row className="justify-content-md-center">
+            <Col xs={12} md={4} className="a-our-services-heading">
+              <Zoom left delay={500}>
+                <h4 className="mediumText blackText">Content Marketing</h4>
+                <p className="blackText montserrat smallestText">
+                  We deliver the perfect message to the <br />
+                  perfect listeners. We write blogs, stories, <br />
+                  newsletter and needed captions.
+                </p>
+              </Zoom>
+            </Col>
+            <Col xs={12} md={4} className="a-our-services-heading">
+              <Zoom left delay={500}>
+                <h4 className="mediumText blackText">Software Development</h4>
+                <p className="blackText montserrat smallestText">
+                  We develop your apps and websites into a <br />
+                  perfect working prototype as we deliver <br />
+                  design, front-end and back-end service.
+                </p>
+              </Zoom>
+            </Col>
+          </Row>
+        </Container>
+      </Container>
+    );
+  }
 }
--- src/components/WhoWeAre/WhoWeAre.js
+++ src/components/WhoWeAre/WhoWeAre.js
@@ -1,83 +1,114 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
-import '../GlobalStyles/global.css';
-import './WhoWeAre.css';
+import "../GlobalStyles/global.css";
+import "./WhoWeAre.css";
 
-import { Container, Col, Row } from 'react-bootstrap';
+import { Container, Col, Row } from "react-bootstrap";
+import Fade from "react-reveal/Fade";
+import Flip from "react-reveal/Flip";
 
-import Person1 from '../../images/person1.png';
-import Person2 from '../../images/person2.png';
-import Forward from '../../images/forward.png';
-
-
+import Person1 from "../../images/person1.png";
+import Person2 from "../../images/person2.png";
+import Forward from "../../images/forward.png";
 
 export default class WhoWeAre extends Component {
-    render() {
-        return (
-            <Container fluid className="whitebg">
-                <Container className="a-wwa-wrapper">
-                    <Col md={{ span: 8, offset: 2 }} sm={{ span: 10, offset: 1 }}>
-                        <Row>
-                            <Col xs={12} md={6} sm={12}>
-                                <h2 className="bigText">
-                                    <span className="blackText">Who </span>
-                                    <span className="green">we </span>
-                                    <span className="blackText">are</span>
-                                </h2>
-                            </Col>
-                            <Col xs={12} md={6} sm={12}>
-                                <div className="a-wwa-caption smallestText blackText b4 montserrat">
-                                    We are a team of great minds to serve our consumers with 
-                                    excellent delivery service to achieve the function of 
-                                    usable products and help our client improve in sales and
-                                    revenue. <br /><br />
-                                                
-                                    <p>We believe not just good, but top-notch branding and
-                                        design keep your products relevant and enables your 
-                                    </p>
-                                    <div>
-                                        <Link to="/" className="a-wwa-readmore green b7 smallestText montserrat">Read more <img src={Forward} alt="forward"/></Link>
-                                    </div>
-                                </div>
-                            </Col>
-                        </Row> 
-                    </Col>
-                    <Row className="pt-4">
-                        <Col  md={2}>
-                        </Col>
-                        <Col  md={8}>
-                            <Row>
-                                <Col md={6} className="image">
-                                    <img src={Person1} alt="Person1" className="image-single" />
-                                </Col>
-                                <Col md={6} className="image">
-                                    <img src={Person2} alt="Person2" className="image-single" />
-                                </Col>
-                            </Row>
-                        </Col>
-                        <Col md={2} sm={8} className="a-wwa-sideline">
-                            <div>
-                                <p className="smallText b7 blackText montserrat">Find us on</p>
-                                <span className="extraSmallText montserrat">
-                                    <li>
-                                        <a href="https://web.facebook.com/Aorthar/" target="blank">Facebook</a>                                
-                                    </li>
-                                    <li>
-                                        <a href="https://www.instagram.com/aorthar/" target="blank">Instagram</a>
-                                    </li>
-                                    <li>
-                                        <a href="https://twitter.com/aorthar" target="blank">Twitter</a>
-                                    </li>
-                                    <li>
-                                        <a href="/" target="blank">Google</a>
-                                    </li>
-                                </span>
-                            </div>
-                        </Col>
-                    </Row>
-                </Container>
-            </Container>
-        )
-    }
+  render() {
+    return (
+      <Container fluid className="whitebg">
+        <Container className="a-wwa-wrapper">
+          <Col md={{ span: 8, offset: 2 }} sm={{ span: 10, offset: 1 }}>
+            <Row>
+              <Col xs={12} md={6} sm={12}>
+                <Fade left>
+                  <h2 className="bigText">
+                    <span className="blackText">Who </span>
+                    <span className="green">we </span>
+                    <span className="blackText">are</span>
+                  </h2>
+                </Fade>
+              </Col>
+              <Col xs={12} md={6} sm={12}>
+                <Fade right>
+                  <div className="a-wwa-caption smallestText blackText b4 montserrat">
+                    We are a team of great minds to serve our consumers with
+                    excellent delivery service to achieve the function of usable
+                    products and help our client improve in sales and revenue.{" "}
+                    <br />
+                    <br />
+                    <p>
+                      We believe not just good, but top-notch branding and
+                      design keep your products relevant and enables your
+                    </p>
+                    <div>
+                      <Link
+                        to="/"
+                        className="a-wwa-readmore green b7 smallestText montserrat"
+                      >
+                        Read more <img src={Forward} alt="forward" />
+                      </Link>
+                    </div>
+                  </div>
+                </Fade>
+              </Col>
+            </Row>
+          </Col>
+          <Row className="pt-4">
+            <Col md={2}></Col>
+            <Col md={8}>
+              <Row>
+                <Col md={6} className="image">
+                  <Fade top>
+                    <img src={Person1} alt="Person1" className="image-single" />
+                  </Fade>
+                </Col>
+                <Col md={6} className="image">
+                  <Fade bottom>
+                    <img src={Person2} alt="Person2" className="image-single" />
+                  </Fade>
+                </Col>
+              </Row>
+            </Col>
+            <Col md={2} sm={8} className="a-wwa-sideline">
+              <Flip top delay={800}>
+                <div>
+                  <p className="smallText b7 blackText montserrat">
+                    Find us on
+                  </p>
+                  <span className="extraSmallText montserrat">
+                    <li>
+                      <a
+                        href="https://web.facebook.com/Aorthar/"
+                        target="blank"
+                      >
+                        Facebook
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="https://www.instagram.com/aorthar/"
+                        target="blank"
+                      >
+                        Instagram
+                      </a>
+                    </li>
+                    <li>
+                      <a href="https://twitter.com/aorthar" target="blank">
+                        Twitter
+                      </a>
+                    </li>
+                    <li>
+                      <a href="/" target="blank">
+                        Google
+                      </a>
+                    </li>
+                  </span>
+                </div>
+              </Flip>
+            </Col>
+          </Row>
+        </Container>
+      </Container>
+    );
+  }
 }
